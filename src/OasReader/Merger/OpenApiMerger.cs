2cbacbb015a70039f38ecb6761580b7b93d45e95
Christian Helle <christian.helle@outlook.com>
2024-01-03 12:41:48 +0100

    Fork OpenAPI Merger code from https://www.nuget.org/packages/dotnet-openapi-merger

---
+++ src/OasReader/Merger/OpenApiMerger.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.OpenApi;
+using Microsoft.OpenApi.Extensions;
+using Microsoft.OpenApi.Models;
+using Microsoft.OpenApi.Services;
+using OasReader.Merger.Visitors;
+using OasReader.OAS;
+
+
+namespace OasReader.Merger
+{
+    public static class OpenApiMerger
+    {
+        public static string Merge(string input, OpenApiDocument document, CancellationToken cancellationToken)
+        {
+            var cache = new Dictionary<string, OpenApiDocument>();
+
+            int missingCount;
+            do
+            {
+                var referenceVisitor = new OpenApiReferenceResolverVisitor(input, cache);
+                var referenceWalker = new OpenApiWalker(referenceVisitor);
+                referenceWalker.Walk(document);
+                referenceVisitor.Cache.UpdateDocument(document);
+            
+                var missingVisitor = new OpenApiMissingReferenceVisitor(document, cache);
+                var missingWalker = new OpenApiWalker(missingVisitor);
+                missingWalker.Walk(document);
+                missingCount = missingVisitor.Cache.Count;
+                missingVisitor.Cache.UpdateDocument(document);
+            }
+            while (missingCount > 0);
+            
+            document.Components.Schemas = document.Components.Schemas
+                .OrderBy(kvp => kvp.Key)
+                .ToDictionary(kvp => kvp.Key, kvp => kvp.Value);
+
+            return input.EndsWith("yaml", StringComparison.OrdinalIgnoreCase) ||
+                   input.EndsWith("yml", StringComparison.OrdinalIgnoreCase)
+                ? document.SerializeAsYaml(OpenApiSpecVersion.OpenApi3_0)
+                : document.SerializeAsJson(OpenApiSpecVersion.OpenApi3_0);
+        }
+    }
+}
---
+++ src/OasReader/Merger/OpenApiReferenceResolverVisitor.cs
@@ -0,0 +1,121 @@
+﻿using Microsoft.OpenApi.Interfaces;
+using Microsoft.OpenApi.Models;
+using Microsoft.OpenApi.Readers;
+using Microsoft.OpenApi.Services;
+
+namespace OasReader.Merger.Visitors
+{
+    public class OpenApiReferenceResolverVisitor : OpenApiVisitorBase
+    {
+        private readonly Dictionary<string, OpenApiDocument> documentCache;
+        private static readonly Lazy<HttpClient> HttpClient = new();
+        private readonly List<FileInfo> files;
+        
+        internal ReferenceCache Cache { get; } = new();
+
+        public OpenApiReferenceResolverVisitor(
+            string input,
+            Dictionary<string, OpenApiDocument> documentCache)
+        {
+            this.documentCache = documentCache;
+
+            files = Directory
+                .GetFiles(Path.GetDirectoryName(input)!, $"*{Path.GetExtension(input)}", SearchOption.AllDirectories)
+                .Select(f => new FileInfo(f))
+                .Where(f => f.Exists)
+                .ToList();
+        }
+
+        public override void Visit(IOpenApiReferenceable referenceable)
+        {
+            if (!(referenceable.Reference?.IsExternal ?? false) ||
+                !TryLoadDocument(referenceable, out var externalDocument) ||
+                externalDocument == null)
+            {
+                return;
+            }
+
+            var localReference = new OpenApiReference
+            {
+                Id = referenceable.Reference!.Id.Split('/').Last(),
+                Type = referenceable.Reference.Type ?? ReferenceType.Schema
+            };
+
+            if (externalDocument.ResolveReference(localReference) is { } reference)
+            {
+                Cache.Add(reference);
+            }
+
+            referenceable.Reference = localReference;
+        }
+
+        private bool TryLoadDocument(IOpenApiReferenceable referenceable, out OpenApiDocument? document)
+        {
+            document = null;
+            var reference = referenceable.Reference?.IsExternal ?? false
+                ? referenceable.Reference.ExternalResource
+                : null;
+            if (reference == null)
+            {
+                return false;
+            }
+
+            if (documentCache.TryGetValue(reference, out OpenApiDocument? value))
+            {
+                document = value;
+                return true;
+            }
+
+            var externalDocument = GetDocument(reference);
+            if (externalDocument == null)
+            {
+                return false;
+            }
+
+            documentCache[reference] = externalDocument;
+            document = documentCache[reference];
+            return true;
+        }
+
+        private OpenApiDocument? GetDocument(string reference)
+        {
+            if (string.IsNullOrWhiteSpace(reference))
+            {
+                return null;
+            }
+
+            if (reference.StartsWith("http", StringComparison.OrdinalIgnoreCase))
+            {
+                return GetDocumentFromStream(
+                    reference,
+                    HttpClient.Value.GetStreamAsync(new Uri(reference)).GetAwaiter().GetResult());
+            }
+
+            var file = files.FirstOrDefault(
+                f => f.FullName.EndsWith(
+                    reference
+                        .Replace('\\', Path.DirectorySeparatorChar)
+                        .Replace('/', Path.DirectorySeparatorChar)));
+
+            if (file == null)
+            {
+                return null;
+            }
+
+            using var fs = file.OpenRead();
+            return GetDocumentFromStream(reference, fs);
+        }
+
+        private static OpenApiDocument? GetDocumentFromStream(string reference, Stream stream)
+        {
+            try
+            {
+                return new OpenApiStreamReader().Read(stream, out var results);
+            }
+            catch
+            {
+                return null;
+            }
+        }
+    }
+}
