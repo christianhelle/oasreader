8807780feda11001ab4a720015cc94d41a6cad2a
Christian Helle <christian.helle@outlook.com>
2026-07-02 12:24:14 +0200

    Cover ContainsExternalReferences with null operation parameter

--- src/OasReader.Tests/OpenApiDocumentExtensionsTests.cs
+++ src/OasReader.Tests/OpenApiDocumentExtensionsTests.cs
@@ -393,6 +393,29 @@ public class OpenApiDocumentExtensionsTests
         sut.ContainsExternalReferences().Should().BeFalse();
     }
 
+    [Fact]
+    public void ContainsExternalReferences_BeFalse_WithOperationNullParameter()
+    {
+        var sut = new OpenApiDocument
+        {
+            Paths = new OpenApiPaths
+            {
+                ["/pets"] = new OpenApiPathItem
+                {
+                    Operations = new Dictionary<HttpMethod, OpenApiOperation>
+                    {
+                        [HttpMethod.Get] = new OpenApiOperation
+                        {
+                            Parameters = new List<IOpenApiParameter> { null! }
+                        }
+                    }
+                }
+            }
+        };
+
+        sut.ContainsExternalReferences().Should().BeFalse();
+    }
+
     [Theory]
     [InlineData("https://developers.intellihr.io/docs/v1/swagger.json")] // GZIP encoded
     [InlineData("http://raw.githubusercontent.com/christianhelle/refitter/main/test/OpenAPI/v3.0/petstore.json")]
