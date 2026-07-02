63f2147bbc27c7e07a256ad8f7ea01f921b0525d
Christian Helle <christian.helle@outlook.com>
2026-07-02 12:19:19 +0200

    Cover ContainsExternalReferences with path item parameter content external ref

--- src/OasReader.Tests/OpenApiDocumentExtensionsTests.cs
+++ src/OasReader.Tests/OpenApiDocumentExtensionsTests.cs
@@ -256,6 +256,32 @@ public class OpenApiDocumentExtensionsTests
         sut.ContainsExternalReferences().Should().BeTrue();
     }
 
+    [Fact]
+    public async Task ContainsExternalReferences_BeTrue_WithPathItemParameterContentExternalRef()
+    {
+        var sut = await LoadDocumentFromTextAsync("""
+            openapi: 3.0.1
+            info:
+              title: Test
+              version: "1.0"
+            paths:
+              /pets:
+                parameters:
+                  - name: PetId
+                    in: query
+                    content:
+                      application/json:
+                        schema:
+                          $ref: 'components.yaml#/components/schemas/Pet'
+                get:
+                  responses:
+                    '200':
+                      description: ok
+            """);
+
+        sut.ContainsExternalReferences().Should().BeTrue();
+    }
+
     [Theory]
     [InlineData("https://developers.intellihr.io/docs/v1/swagger.json")] // GZIP encoded
     [InlineData("http://raw.githubusercontent.com/christianhelle/refitter/main/test/OpenAPI/v3.0/petstore.json")]
