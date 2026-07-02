1235fc1e3dbfb013aa159e045baaa34bd0efc931
Christian Helle <christian.helle@outlook.com>
2026-07-02 12:16:01 +0200

    Cover ContainsExternalReferences with response header external ref

--- src/OasReader.Tests/OpenApiDocumentExtensionsTests.cs
+++ src/OasReader.Tests/OpenApiDocumentExtensionsTests.cs
@@ -212,6 +212,29 @@ public class OpenApiDocumentExtensionsTests
         sut.ContainsExternalReferences().Should().BeTrue();
     }
 
+    [Fact]
+    public async Task ContainsExternalReferences_BeTrue_WithResponseHeaderExternalRef()
+    {
+        var sut = await LoadDocumentFromTextAsync("""
+            openapi: 3.0.1
+            info:
+              title: Test
+              version: "1.0"
+            paths:
+              /pets:
+                get:
+                  responses:
+                    '200':
+                      description: ok
+                      headers:
+                        X-RateLimit:
+                          schema:
+                            $ref: 'components.yaml#/components/schemas/Pet'
+            """);
+
+        sut.ContainsExternalReferences().Should().BeTrue();
+    }
+
     [Theory]
     [InlineData("https://developers.intellihr.io/docs/v1/swagger.json")] // GZIP encoded
     [InlineData("http://raw.githubusercontent.com/christianhelle/refitter/main/test/OpenAPI/v3.0/petstore.json")]
